5c5bcd7ffa266dbd0d3812e04e135c90fca0b271
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2024-03-09 01:25:48 +0600

    fix build issue

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,93 +1,14 @@
-"use client";
+import ProfilePage from "@/components/ProfilePage";
+import { cookies } from "next/headers";
 
-import { useCookies } from "next-client-cookies";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-
-export default function Page() {
-  const [sessionList, setSessionList] = useState<any>([]);
-  const [loading, setLoading] = useState(false);
-  const cookies: any = useCookies();
-
-  const user: any = JSON.parse(cookies.get("user"));
-
-  const getCheckoutSessions = async () => {
-    setLoading(true);
-    const result = await fetch("/api/getSessionCheckouts");
-
-    const data = await result.json();
-
-    setSessionList(
-      data?.data.filter((data: any) => {
-        return data?.customer_email === user?.email;
-      })
-    );
-
-    setLoading(false);
-
-    console.log(data?.data);
-
-    // console.log(user.id);
-  };
-
-  useEffect(() => {
-    getCheckoutSessions();
-    console.log(user);
-  }, []);
+export default function page() {
+  const cookieStore = cookies();
+  const userInfo: any = cookieStore.get("user");
+  const user = JSON.parse(userInfo);
 
   return (
     <div className="container min-h-screen w-full py-12">
-      <h2 className="font-bold text-xl mb-3">My Plans:</h2>
-      {loading ? (
-        <p>Please wait...</p>
-      ) : (
-        <>
-          {sessionList?.length > 0 ? (
-            sessionList.map((item: any, indx: number) => {
-              // if (item?.customer_email === user?.email) {
-              return (
-                <div className="p-3 mb-4 border-2 rounded" key={indx}>
-                  <>
-                    {item?.lines?.data
-                      .map((itm: any, index: number) => {
-                        return (
-                          <div className="flex justify-between" key={index}>
-                            <p>
-                              {itm?.type === "subscription" &&
-                              index === item?.lines?.data.length - 1 ? (
-                                <h6 className="mb-2">
-                                  Plan: {itm?.description.split("×")[1]}
-                                </h6>
-                              ) : (
-                                `Extra item ${
-                                  Math.abs(
-                                    index - item?.lines?.data.length + 2
-                                  ) + 1
-                                }: ` + itm?.description
-                              )}
-                            </p>
-                          </div>
-                        );
-                      })
-                      .reverse()}
-                    <div className="mt-3">
-                      <Link
-                        href={item?.invoice_pdf}
-                        className="bg-green-600 hover:bg-green-700 text-white px-2 py-1 rounded"
-                      >
-                        Download Invoice
-                      </Link>
-                    </div>
-                  </>
-                </div>
-              );
-              // }
-            })
-          ) : (
-            <p>No plan available</p>
-          )}
-        </>
-      )}
+      <ProfilePage user={user} />
     </div>
   );
 }
