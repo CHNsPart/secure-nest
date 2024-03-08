287aad70ebe5913b95ac3a4af2485c01ecc9346d
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2024-03-09 01:32:55 +0600

    fix build issue

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,8 @@ import { cookies } from "next/headers";
 export default function page() {
   const cookieStore = cookies();
   const userInfo: any = cookieStore.get("user");
-  const user = JSON.parse(userInfo);
+  // console.log(userInfo);
+  const user = JSON?.parse(userInfo?.value);
 
   return (
     <div className="container min-h-screen w-full py-12">
